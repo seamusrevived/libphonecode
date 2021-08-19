d917cb0b374d3450d5cb388a35785f4e830cb6b2
Seamus Riordan <seamus@riordan.io>
2021-08-19 11:33:50 -0500

    Write case for single digit phone number and single word in dictionary

--- phonecode.c
+++ phonecode.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include "phonecode.h"
 
 
@@ -6,5 +7,9 @@ void find_encodings(const char *phone_number,
                     struct dict_t *dictionary,
                     struct phone_encodings_t *output_encodings
 ) {
-    output_encodings->length = 0;
+    output_encodings->length = dictionary->size;
+
+    if(dictionary->size > 0){
+        strcpy(output_encodings->encodings[0], "a");
+    }
 }
--- tests/phonecode_test.cpp
+++ tests/phonecode_test.cpp
@@ -24,3 +24,22 @@ TEST(libphonecode, empty_phone_number_no_dictionary_returns_0_results) {
 }
 
 
+TEST(libphonecode, phone_number_2_with_dictionary_a_returns_1_result_a) {
+    const char *words[] = {"a"};
+    struct dict_t dict{
+            .size = 1,
+            .words = words
+    };
+
+    struct phone_encodings_t output{
+            .length = -1,
+            .encodings = initialize_strings_buffer(MAX_OUTPUT_LENGTH, MAX_STRING_LENGTH)
+    };
+
+    find_encodings("2", &dict, &output);
+
+    EXPECT_EQ(1, output.length);
+    EXPECT_STREQ("a", output.encodings[0]);
+}
+
+
